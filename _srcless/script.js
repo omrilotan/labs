e033294134b53346025242f6ef1593ed4ae59a88
watermelonbunny <omri@hamadgera.com>
2013-07-01 02:20:47 -0700

    add access to window, add owner object

--- _srcless/script.js
+++ _srcless/script.js
@@ -1,11 +1,17 @@
-(function (win, namespace, undefined) {
+var srcless = (function (window, document) {
     var frame = function (wrapper) {
         this.wrapper = typeof wrapper === "string" ? document.querySelector(wrapper) : wrapper;
         this.frame = document.createElement('iframe');
-        this.wrapper.appendChild(this.frame)
+        this.wrapper.appendChild(this.frame);
+        this.win = this.frame.contentWindow;
         this.doc = this.frame.contentWindow.document;
+
+        this.win.owner = {
+            win: window,
+            doc: document
+        };
     };
     frame.prototype = {};
 
-    win[namespace] = frame;
-}(window, "srcless"));
+    return frame;
+}(window, document));
