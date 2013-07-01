54c1f92e5e46c5f6639a35c9107264dd16fc7991
watermelonbunny <omri@hamadgera.com>
2013-07-01 08:23:06 -0700

    audio player fix ups

--- _audioPlayer/script.js
+++ _audioPlayer/script.js
@@ -1,19 +1,23 @@
 var soundPlayer = (function () {
     "use strict";
     var collection = {},
-        Sound,
-        embed = null;
-    Sound = function (name) {
-        var source;
-
-        this.name = name;
-        this.audio = document.createElement("audio");
-        this.empty = true;
-    };
-
+        fallbackObject = null,
+        addParam = function (name, value) {
+            var param = document.createElement("param");
+            param.name = name;
+            param.value = value;
+            return param;
+        },
+        Sound = function (name) {
+            this.name = name;
+            this.audio = document.createElement("audio");
+            this.first = null;
+        };
     Sound.prototype.src = function (url, type) {
-        this.empty = false;
         var source = document.createElement("source");
+        if (this.first === null) {
+            this.first = { url: url, type: type };
+        }
         source.src = url;
         if (type) {
             source.type = type;
@@ -21,28 +25,38 @@ var soundPlayer = (function () {
         this.audio.appendChild(source);
         return this;
     };
-
     Sound.prototype.play = function () {
-        if (!this.empty && this.isWorking()) {
+        var embed;
+        if (this.first === null) {
+            throw new Error("Sound Player Error: no file was supplied");
+        }
+        if (this.isWorking()) {
             this.audio.play();
         } else {
-            if (embed !== null && embed.parentNode) {
-                embed.parentNode.removeChild(embed);
+            if (fallbackObject !== null && fallbackObject.parentNode) {
+                fallbackObject.parentNode.removeChild(fallbackObject);
             }
+            fallbackObject = document.createElement("object")
+            fallbackObject.appendChild(addParam("autostart", "true"));
+            fallbackObject.appendChild(addParam("autoplay", "true"));
+            fallbackObject.appendChild(addParam("controller", "false"));
+            fallbackObject.appendChild(addParam("hidden", "true"));
             embed = document.createElement("embed");
             embed.hidden = "true";
             embed.autostart = "true";
-            embed.src = this.name + ".mp3";
-            document.body.appendChild(embed);
+            embed.autoplay = "true";
+            embed.controller = "false";
+            embed.src = this.first.url;
+            embed.type = this.first.type;
+            fallbackObject.appendChild(embed);
+            document.body.appendChild(fallbackObject);
         }
         return this;
     };
-
     Sound.prototype.isWorking = function () {
         return typeof this.audio.paused === "boolean" &&
                 typeof this.audio.ended === "boolean";
     };
-
     return function (name) {
         if (!collection[name]) {
             collection[name] = new Sound(name);
