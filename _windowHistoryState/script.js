6353d229a1cff838a4361a0287054660e64a5e5d
watermelonbunny <omri@hamadgera.com>
2013-07-04 03:10:01 -0700

    page navigation oriented window.history wrapper

--- _windowHistoryState/script.js
+++ _windowHistoryState/script.js
@@ -1,39 +1,97 @@
 var pageManager = (function (window) {
     var exports = {},
         callbacks = [],
-        pop = function (event) {
+        onPopState = function (event) {
             var i = 0,
                 len = callbacks.length;
+            event.timeStamp = event.timeStamp || (new Date()).getTime();
+            event.type = event.type || "popstate";
+
             for (; i < len; i++) {
                 callbacks[i](event);
             }
+        },
+
+        // combine objects
+        combine = function () {
+            var i = 0,
+                len = arguments.length,
+                combined = {},
+                nue,
+                key;
+            for (; i < len; i++) {
+                nue = arguments[i];
+                for (key in nue) {
+                    if (nue.hasOwnProperty(key)) {
+                        combined[key] = nue[key];
+                    }
+                }
+            }
+            return combined;
         };
+
+    // add the event listener
     if (typeof window.addEventListener === "function") {
-        window.addEventListener("popstate", pop);
+        window.addEventListener("popstate", onPopState);
     } else if (typeof window.attachEvent === "function") {
-        window.attachEvent("onpopstate", pop);
+        window.attachEvent("onpopstate", onPopState);
     } else {
-        window.onpopstate = pop;
+        window.onpopstate = onPopState;
     }
 
-    exports.go = function (name, data) {
-        data = typeof data === "object" ? data : null;
-        window.history.pushState(data, name, "#!/" + name);
-        pop({
-            state: data,
-            timeStamp: (new Date()).getTime(),
-            type: "popstate"
-        });
+    // simulate emitting the popstate event
+    exports.state = function () {
+        onPopState({ state: window.history.state });
         return this;
     };
 
-    exports.listen = function (fn) {
-        callbacks.push(fn);
+    // Apply all window.history methods
+    exports.push = function (data) {
+        if (typeof data !== "object") {
+            throw new Error("pageManager Error: pushing a state requires a data object");
+        }
+        window.history.pushState(data, data.name, "#!/" + data.name);
+        onPopState({ state: data });
         return this;
     };
-
+    exports.replace = function (data) {
+        if (typeof data !== "object") {
+            throw new Error("pageManager Error: replacing a state requires a data object");
+        }
+        window.history.replaceState(data, data.name, "#!/" + data.name);
+        onPopState({ state: data });
+        return this;
+    };
     exports.back = function () {
         window.history.back();
+        return this;
+    };
+    exports.forward = function () {
+        window.history.forward();
+        return this;
+    };
+    exports.go = function (num) {
+        window.history.go(num);
+        return this;
+    };
+    exports.getState = function () {
+        return window.history.state;
+    };
+    exports.getLength = function () {
+        return window.history.length;
+    };
+
+    // add data to current
+    exports.add = function (data) {
+        if (typeof data !== "object") {
+            throw new Error("pageManager Error: adding to state requires a data object");
+        }
+        return this.replace(combine(window.history.state, data));
+    };
+
+    // add an event listener method to "popstate"
+    exports.listen = function (fn) {
+        callbacks.push(fn);
         return this;
     };
 
