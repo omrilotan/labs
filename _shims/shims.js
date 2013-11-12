facb3388e0f1a6741e0da685f2656cca15c62c6a
watermelonbunny <omri@hamadgera.com>
2013-11-12 20:44:55 +0200

    Element: add and remove listeners

--- _shims/shims.js
+++ _shims/shims.js
@@ -1,19 +1,5 @@
 // Build Date: 2013-11-12
 
-if (typeof Array.prototype.indexOf !== "function"){
-	Array.prototype.indexOf = function indexOf (what, i) {
-	    i = i || 0;
-	    var len = this.length;
-	    while (i < len) {
-	        if (this[i] === what) {
-	            return i;
-	        }
-	        ++i;
-	    }
-	    return -1;
-	};
-}
-
 if (typeof String.prototype.trim !== "function"){
 	String.prototype.trim = function trim () {
 	    return this.replace(/^\s+|\s+$/g, "");
@@ -30,36 +16,49 @@ if (typeof Array.prototype.forEach !== "function"){
 	};
 }
 
-if (typeof Array.prototype.reduce !== "function"){
-	Array.prototype.reduce = function reduce(callback, context) {
-	    if (this === null || this === undefined) {
-	        throw new TypeError("Array.prototype.reduce called on null or undefined");
+if (typeof Element.prototype.matches !== "function"){
+	Element.prototype.matches =
+	        Element.prototype.matchesSelector ||
+	        Element.prototype.mozMatchesSelector ||
+	        Element.prototype.webkitMatchesSelector ||
+	        Element.prototype.msMatchesSelector ||
+	        Element.prototype.oMatchesSelector || function (query) {
+	            var collection = document.querySelectorAll(query);
+	            return [].indexOf.call(collection, this);
+	        };
+}
+
+if (typeof Element.prototype.addEventListener !== "function"){
+	Element.prototype.addEventListener = function (type, method /* useCapture */) {
+	    if (typeof this.attachEvent === "function") {
+	        this.attachEvent("on" + type, method);
+	    } else {
+	        this["on" + type] = method;
 	    }
-	    if (typeof callback !=="function") {
-	        throw new TypeError(callback + " is not a function");
+	};
+}
+
+if (typeof Element.prototype.removeEventListener !== "function"){
+	Element.prototype.removeEventListener = function (type, method /* useCapture */) {
+	    if (typeof this.detachEvent === "function") {
+	        this.detachEvent("on" + type, method);
+	    } else {
+	        this["on" + type] = null;
 	    }
-	    var i = 0,
-	        len = this.length,
-	        value,
-	        isValueSet = false;
-	    if (arguments.length > 1) {
-	        value = context;
-	        isValueSet = true;
-	    }
-	    for (; len > i; ++i) {
-	        if (this.hasOwnProperty(i)) {
-	            if (isValueSet) {
-	                value = callback(value, this[i], i, this);
-	            } else {
-	                value = this[i];
-	                isValueSet = true;
-	            }
+	};
+}
+
+if (typeof Array.prototype.indexOf !== "function"){
+	Array.prototype.indexOf = function indexOf (what, i) {
+	    i = i || 0;
+	    var len = this.length;
+	    while (i < len) {
+	        if (this[i] === what) {
+	            return i;
 	        }
+	        ++i;
 	    }
-	    if (!isValueSet) {
-	        throw new TypeError("Reduce of empty array with no initial value");
-	    }
-	    return value;
+	    return -1;
 	};
 }
 
@@ -71,14 +70,22 @@ if (typeof Object.prototype.hasOwnProperty !== "function"){
 	};
 }
 
-if (typeof Element.prototype.matches !== "function"){
-	Element.prototype.matches =
-	        Element.prototype.matchesSelector ||
-	        Element.prototype.mozMatchesSelector ||
-	        Element.prototype.webkitMatchesSelector ||
-	        Element.prototype.msMatchesSelector ||
-	        Element.prototype.oMatchesSelector || function (query) {
-	            var collection = document.querySelectorAll(query);
-	            return [].indexOf.call(collection, this);
+if (typeof Function.prototype.bind !== "function"){
+	Function.prototype.bind = function bind (that) {
+	    if (typeof this !== "function") {
+	        // closest thing possible to the ECMAScript 5 internal IsCallable function
+	        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+	    }
+	    var args = Array.prototype.slice.call(arguments, 1), 
+	        original = this, 
+	        dummy = function () {},
+	        bound = function () {
+	            return original.apply(this instanceof dummy && that ?
+	                    this : that,
+	                    args.concat(Array.prototype.slice.call(arguments)));
 	        };
+	    dummy.prototype = this.prototype;
+	    bound.prototype = new dummy();
+	    return bound;
+	};
 }
