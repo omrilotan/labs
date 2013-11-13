6d8b678eea362e22707b30fbd35beda84924060e
watermelonbunny <omri@hamadgera.com>
2013-11-13 07:24:58 +0200

    remove shims. off to another reposetory

--- _shims/lib/Element/addEventListener.js
+++
@@ -1,7 +0,0 @@
-Element.prototype.addEventListener = function (type, method /* useCapture */) {
-    if (typeof this.attachEvent === "function") {
-        this.attachEvent("on" + type, method);
-    } else {
-        this["on" + type] = method;
-    }
-};
--- _shims/lib/Element/removeEventListener.js
+++
@@ -1,7 +0,0 @@
-Element.prototype.removeEventListener = function (type, method /* useCapture */) {
-    if (typeof this.detachEvent === "function") {
-        this.detachEvent("on" + type, method);
-    } else {
-        this["on" + type] = null;
-    }
-};
