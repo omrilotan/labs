b2a9de0102ae245a075b023d616d5273ad903c5e
watermelonbunny <omri@hamadgera.com>
2013-11-22 09:57:51 +0200

    small changes

--- KeyboardShortcuts/KeyboardShortcuts.js
+++ KeyboardShortcuts/KeyboardShortcuts.js
@@ -1,11 +1,11 @@
 /* props: http://www.openjs.com/scripts/events/keyboard_shortcuts/ (Version 2.01.B) */
-KeyboardShortcuts = {
-    'all_shortcuts': {}, //All the shortcuts are stored in this array
-    'add': function (shortcut_combination, callback, options) {
+var KeyboardShortcuts = {
+    "all_shortcuts": {}, //All the shortcuts are stored in this array
+    "add": function (shortcut_combination, callback, options) {
         shortcut_combination = shortcut_combination.toLowerCase();
         //Provide a set of default options
         var default_options = {
-            type: 'keydown',
+            type: "keydown",
             propagate: false,
             disable_in_input: false,
             target: document,
@@ -21,7 +21,7 @@ KeyboardShortcuts = {
             }
         }
         var ele = options.target;
-        if (typeof options.target === 'string') {
+        if (typeof options.target === "string") {
             ele = document.querySelector(options.target);
         }
         //The function to be called at keypress
@@ -62,27 +62,27 @@ KeyboardShortcuts = {
                 },
                 //Special Keys - and their codes
                 special_keys = {
-                    'esc': 27, 'escape': 27,
-                    'tab': 9,
-                    'spacebar': 32, 'space_bar': 32, 'space': 32,
-                    'return': 13, 'enter': 13,
-                    'backspace': 8,
-                    'scrolllock': 145, 'scroll_lock': 145, 'scroll': 145,
-                    'capslock': 20, 'caps_lock': 20, 'caps': 20,
-                    'numlock': 144, 'num_lock': 144, 'num': 144,
-                    'pause': 19, 'break': 19,
-                    'insert': 45, 'home': 36, 'delete': 46, 'end': 35,
-                    'pageup': 33, 'page_up': 33, 'pu': 33,
-                    'pagedown': 34, 'page_down': 34, 'pd': 34,
-                    'left': 37, 'up': 38, 'right': 39, 'down': 40,
-                    'f1': 112, 'f2': 113, 'f3': 114, 'f4': 115, 'f5': 116, 'f6': 117, 'f7': 118, 'f8': 119, 'f9': 120, 'f10': 121, 'f11': 122, 'f12': 123
+                    "esc": 27, "escape": 27,
+                    "tab": 9,
+                    "spacebar": 32, "space_bar": 32, "space": 32,
+                    "return": 13, "enter": 13,
+                    "backspace": 8,
+                    "scrolllock": 145, "scroll_lock": 145, "scroll": 145,
+                    "capslock": 20, "caps_lock": 20, "caps": 20,
+                    "numlock": 144, "num_lock": 144, "num": 144,
+                    "pause": 19, "break": 19,
+                    "insert": 45, "home": 36, "delete": 46, "end": 35,
+                    "pageup": 33, "page_up": 33, "pu": 33,
+                    "pagedown": 34, "page_down": 34, "pd": 34,
+                    "left": 37, "up": 38, "right": 39, "down": 40,
+                    "f1": 112, "f2": 113, "f3": 114, "f4": 115, "f5": 116, "f6": 117, "f7": 118, "f8": 119, "f9": 120, "f10": 121, "f11": 122, "f12": 123
                 };
             if (options.disable_in_input === true) {
                 //Don't enable shortcut keys in Input, Textarea fields
                 element = event.target ? event.target : event.srcElement;
                 element = element.nodeType === 3 ? element.parentNode : element;
                 tagName = element.tagName.toUpperCase();
-                if (tagName === 'INPUT' || tagName === 'TEXTAREA' || tagName === 'SELECT' || tagName === 'OPTION') {
+                if (tagName === "INPUT" || tagName === "TEXTAREA" || tagName === "SELECT" || tagName === "OPTION") {
                     return;
                 }
             }
@@ -127,16 +127,16 @@ KeyboardShortcuts = {
             for (; i < loops; i++) {
                 key = keys[i];
                 //Modifiers
-                if (key === 'ctrl' || key === 'control') {
+                if (key === "ctrl" || key === "control") {
                     keyPressed++;
                     modifiers.ctrl.wanted = true;
-                } else if (key === 'shift') {
+                } else if (key === "shift") {
                     keyPressed++;
                     modifiers.shift.wanted = true;
-                } else if (key === 'alt') {
+                } else if (key === "alt") {
                     keyPressed++;
                     modifiers.alt.wanted = true;
-                } else if (key === 'meta') {
+                } else if (key === "meta") {
                     keyPressed++;
                     modifiers.meta.wanted = true;
                 } else if (key.length > 1) { //If it is a special key
@@ -189,13 +189,13 @@ KeyboardShortcuts = {
         if (ele.addEventListener) {
             ele.addEventListener(options.type, func, false);
         } else if (ele.attachEvent) {
-            ele.attachEvent('on' + options.type, func);
+            ele.attachEvent("on" + options.type, func);
         } else {
-            ele['on' + options.type] = func;
+            ele["on" + options.type] = func;
         }
     },
     //Remove the shortcut - just specify the shortcut and I will remove the binding
-    'remove': function (shortcut_combination) {
+    "remove": function (shortcut_combination) {
         shortcut_combination = shortcut_combination.toLowerCase();
         var binding = this.all_shortcuts[shortcut_combination];
         delete (this.all_shortcuts[shortcut_combination]);
@@ -206,16 +206,16 @@ KeyboardShortcuts = {
         var ele = binding.target;
         var callback = binding.callback;
         if (ele.detachEvent) {
-            ele.detachEvent('on' + type, callback);
+            ele.detachEvent("on" + type, callback);
         } else if (ele.removeEventListener) {
             ele.removeEventListener(type, callback, false);
         } else {
-            ele['on' + type] = false;
+            ele["on" + type] = false;
         }
     }
 };
 /* IMPLEMENT
 KeyboardShortcuts.add("Ctrl+z",function() {
 	alert("undo feature");
-},{ 'type': 'keydown', 'propagate': false, 'disable_in_input': true, 'target': document });
+},{ "type": "keydown", "propagate": false, "disable_in_input": true, "target": document });
 */
