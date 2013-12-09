0c4c474d15489218ad24570bebb6ea0249ac03c8
watermelonbunny <omri@hamadgera.com>
2013-12-09 09:42:59 +0200

    json2xml method

--- _JSON2XML/json2xml.js
+++ _JSON2XML/json2xml.js
@@ -1,48 +1,43 @@
 var JSON2XML = (function __JSON2XML__ () {
     "use strict";
-    var wrap = function JSON2XML$_wrap (tag, value) {
-        return "<" + tag + ">" +
-                value +
-                "</" +  tag + ">";
-    };
+    var wrap = function JSON2XML$_wrap (value, tag) {
 
-    // Interface
-    var exports = function JSON2XML (obj, rootTag) {
-        var xml = [],
-            key,
-            nKey,
-            value;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                value = obj[key];
-                if (value instanceof Array &&
-                        typeof value === "object") {
-
+            // <tag>value</tag> representation
+            return "<" + tag + ">" +
+                    value +
+                    "</" +  tag + ">";
+        },
+        
+        // Interface
+        exports = function JSON2XML (value, tag) {
+            var xml = [],
+                key;
+            if (typeof value === "object") {
+                if (value instanceof Array) {
+                    
                     // Push an array of XML style objects
-                    for (nKey in value) {
-
-                        // Recursion
-                        xml.push(exports(value[nKey]));
-                    }
-                } else if (value instanceof Object &&
-                        typeof value === "object") {
+                    value.forEach(function JSON2XML$arrayIterator (item) {
+                        xml.push(exports(item));
+                    });
+                } else if (value instanceof Object) {
 
                     // Push a new XML sub-tree with a sub-root tag of the current key
-                    xml.push(wrap(key, exports(value)));
-                } else {
-
-                    // Simple <tag>value</tag> representation
-                    xml.push(wrap(key, value));
+                    for (key in value) {
+                        if (value.hasOwnProperty(key)) {
+                            xml.push(exports(value[key], key));
+                        }
+                    }
                 }
+            } else {
+                xml.push(value);
             }
-        }
-        return rootTag ?
-                
-                // Wrap with a root tag
-                wrap(rootTag, xml.join("")) :
-
-                // Don't wrap with a root tag
-                xml.join("");
-    };
+            return tag ?
+                    
+                    // Wrap with a root tag
+                    wrap(xml.join(""), tag) :
+                    
+                    // Don't wrap with a root tag
+                    xml.join("");
+        };
     return exports;
 })();
